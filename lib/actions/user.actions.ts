d851669e301399a4afe658872c8d98bf1c2a67f1
sunghye <leeato1107@naver.com>
2025-12-04 13:41:46 +0900

    5nd

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,51 @@
 'use server'
 
-import { signIn, signOut } from '@/auth'
-import { signInFormSchema } from '../validator'
+import { auth, signIn, signOut } from '@/auth'
+import { signInFormSchema, signUpFormSchema } from '../validator'
+import { formatError } from '../utils'
+import { hashSync } from 'bcrypt-ts-edge'
+import db from '@/db/drizzle'
+import { users } from '@/db/schema'
+import { count, desc, eq } from 'drizzle-orm'
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
+import { PAGE_SIZE } from '../constants'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 
+// USER
+export async function signUp(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      confirmPassword: formData.get('confirmPassword'),
+      password: formData.get('password'),
+    })
+    const values = {
+      id: crypto.randomUUID(),
+      ...user,
+      password: hashSync(user.password, 10),
+    }
+    await db.insert(users).values(values)
+    await signIn('credentials', {
+      email: user.email,
+      password: user.password,
+    })
+    return { success: true, message: 'User created successfully' }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
+    }
+    return {
+      success: false,
+      message: formatError(error).includes(
+        'duplicate key value violates unique constraint "user_email_idx"'
+      )
+        ? 'Email is already exist'
+        : formatError(error),
+    }
+  }
+}
 export async function signInWithCredentials(
   prevState: unknown,
   formData: FormData
@@ -14,7 +56,7 @@ export async function signInWithCredentials(
       password: formData.get('password'),
     })
     await signIn('credentials', user)
-    return { success: true, message: 'Sig in successfully' }
+    return { success: true, message: 'Sign in successfully' }
   } catch (error) {
     if (isRedirectError(error)) {
       throw error
@@ -23,6 +65,79 @@ export async function signInWithCredentials(
   }
 }
 
+export const SignInWithEmail = async (formData: any) => {
+  await signIn('email', formData)
+}
+
+export const SignInWithGoogle = async () => {
+  await signIn('google')
+}
+
 export const SignOut = async () => {
   await signOut()
+}
+// GET
+export async function getAllUsers({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number
+  page: number
+}) {
+  const data = await db.query.users.findMany({
+    orderBy: [desc(users.createdAt)],
+    limit,
+    offset: (page - 1) * limit,
+  })
+  const dataCount = await db.select({ count: count() }).from(users)
+  return {
+    data,
+    totalPages: Math.ceil(dataCount[0].count / limit),
+  }
+}
+
+export async function getUserById(userId: string) {
+  const user = await db.query.users.findFirst({
+    where: (users, { eq }) => eq(users.id, userId),
+  })
+  if (!user) throw new Error('User not found')
+  return user
+}
+
+// DELETE
+
+export async function deleteUser(id: string) {
+  try {
+    await db.delete(users).where(eq(users.id, id))
+    revalidatePath('/admin/users')
+    return {
+      success: true,
+      message: 'User deleted successfully',
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+
+export async function updateProfile(user: { name: string; email: string }) {
+  try {
+    const session = await auth()
+    const currentUser = await db.query.users.findFirst({
+      where: (users, { eq }) => eq(users.id, session?.user.id!),
+    })
+    if (!currentUser) throw new Error('User not found')
+    await db
+      .update(users)
+      .set({
+        name: user.name,
+      })
+      .where(eq(users.id, currentUser.id))
+
+    return {
+      success: true,
+      message: 'User updated successfully',
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
 }
